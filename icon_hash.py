4b50cc9d573dac41bca2d7ef65ebbbdc53724ec2
Tycho101 <59411519+tycho1101@users.noreply.github.com>
2024-02-28 15:40:52 +0800

    Update icon_hash.py

--- icon_hash.py
+++ icon_hash.py
@@ -1,8 +1,8 @@
 import mmh3
 import requests
 import codecs
- 
-response = requests.get('https://example.com/favicon.ico')
-favicon = codecs.encode(response.content,"base64")
+
+response = requests.get('https://example.com/favicon.ico', verify=False)
+favicon = codecs.encode(response.content, "base64")
 hash = mmh3.hash(favicon)
 print(hash)
